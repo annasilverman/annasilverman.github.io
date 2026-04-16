d7d8dbb5d8d37cc403e67720e01710a7096be1d7
Anna <112361653+annasilverman@users.noreply.github.com>
2026-04-16 05:54:16 +0000

    concentration finished

--- Concentration/scripts.js
+++ Concentration/scripts.js
@@ -1,5 +1,35 @@
 
+//track number of turns
+let playerClicks = 0;
+
+
+//clear clicked classes
+function clearClicked(){
+    let allClickedCards = document.querySelectorAll(".clicked");
+    for (let eachCard of allClickedCards) {
+        eachCard.classList.remove("clicked");
+    }
+
+    //increases player turns by 1
+    playerClicks++;
+    document.querySelector("#turnCount span").innerHTML = playerClicks;
+
+    //checks for winning
+    let allCards = document.querySelectorAll(".card");
+    let matchedCards = document.querySelectorAll(".matched");
+    if(allCards.length == matchedCards.length){
+        //player has matched all cards
+        document.querySelector("#winning").innerHTML = "You won!";
+        document.querySelector("#winning").style.display = "block";
+        console.log("won game");
+    }
+}
+
 function flipCard() {
+
+    //makes matches unclickable
+    if (!this.classList.contains("matched")){
+
     let allClickedCards = document.querySelectorAll(".clicked");
 
     //only lets user click if they have clicked less than two cards
@@ -14,16 +44,22 @@ function flipCard() {
     allClickedCards = document.querySelectorAll(".clicked");
 
     if(allClickedCards.length == 2){
-
+  
+//going to forgo the variable here because i like how the card doesnt flip during the transition
         let card1 = allClickedCards[0].classList.toString();
         let card2 = allClickedCards[1].classList.toString();
 
         if(card1==card2){
             console.log("it's a match!");
+            allClickedCards[0].classList.add("matched");
+            allClickedCards[1].classList.add("matched");
+            window.setTimeout(clearClicked, 2000);
         }else{
             console.log("not a macth :(")
+            window.setTimeout(clearClicked, 1000);
         }
     }
+    }   
 
 }
 
