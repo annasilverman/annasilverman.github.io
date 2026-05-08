7134af81a219f5e761a367dce62dfb714b5038b4
Anna <112361653+annasilverman@users.noreply.github.com>
2026-05-08 01:10:14 +0000

    made create cards function and changed some css to look better

--- majorProject/scripts.js
+++ majorProject/scripts.js
@@ -1,5 +1,8 @@
+
 //im scared
-const url = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number,image_id";
+
+//this url will determine the random api 
+const url = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number,image_idhttps://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number,image_id&limit=20";
 let random = document.querySelector('.randomart');
 
 async function getData(url) {
@@ -16,6 +19,8 @@ async function getData(url) {
     }
 }
 
+
+//Takes a random 
 function randpiece(randomNumber){
     let randomArtNum = Math.floor(Math.random() * randomNumber.data.length);
     console.log(randomArtNum);
@@ -41,6 +46,51 @@ function randomArt(artObject){
 
 
     //big shoutout to this video for unerstanding the link and with the random art: https://www.youtube.com/watch?v=L8bCI0_u3As
+}
+
+function createcards(artObject){
+   let results = artObject.data;
+   let artResults = document.querySelector(".artResults");
+
+   //this makes the things display
+   results.forEach((result) =>{
+    //adds the card into the main, card needs to be appended to the html 
+    let card = document.createElement('div');
+    card.className = 'artCard'; 
+
+    //needs to be appended to the card
+    let img = document.createElement('img');
+    img.className = 'apiimg';
+    img.src= `https://www.artic.edu/iiif/2/${result.image_id}/full/843,/0/default.jpg`;
+    card.appendChild(img);
+
+    //creates title
+    let title = document.createElement('div');
+    title.className = 'artPeice';
+    title.innerHTML = result.title;
+    card.appendChild(title);
+
+    //creates artist
+    let artist = document.createElement('div');
+    artist.className = 'artistName';
+    artist.innerHTML = result.artist_display;
+    card.appendChild(artist);
+
+    //creates date
+    let date = document.createElement('div');
+    date.className = 'date';
+    date.innerHTML = result.date_display;
+    card.appendChild(date);
+
+    //creates id number
+    let id = document.createElement('div');
+    id.className = 'description';
+    id.innerHTML = result.id;
+    card.appendChild(id);
+//puts everything into a the artcard div
+    artResults.appendChild(card);
+
+   });
 }
 
 
@@ -76,6 +126,7 @@ document.addEventListener("DOMContentLoaded", function () {
     getData(url).then(function (result){
         console.log(result);  //I NEED YOU TO KNOW I DID THIS MY FIRST TRY
         randomArt(result);
+        createcards(result);
 
         //changing random art with the button 
         document.querySelector(".mainButton").addEventListener("click", function() {
@@ -83,8 +134,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
- 
-
-
 
 });
+
